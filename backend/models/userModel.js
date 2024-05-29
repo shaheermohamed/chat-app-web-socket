68b984282c838c8397a1bcce8609dce569979b4c
Mohamed Shaheer <shaheer@coderythm.com>
2024-05-29 22:27:41 +0400

    feat: api integrated with login and signup page

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const userSchema = mongoose.Schema(
   {
-    username: {
+    name: {
       type: String,
       required: true,
     },
     email: {
       type: String,
       required: true,
+      unique: true,
     },
     password: {
       type: String,
@@ -15,7 +17,6 @@ const userSchema = mongoose.Schema(
     },
     pic: {
       type: String,
-      required: true,
       default:
         "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
@@ -24,6 +25,18 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,23 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 
 const connectDB = require("./config/db");
-const colors = require("colors")
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const colors = require("colors");
 const app = express();
 dotenv.config();
 
 connectDB();
 app.use(cors());
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   console.log("hello world");
   res.status(200).send("API is running");
 });
+
+app.use("/api/user", userRoutes);
 
 app.get("/api/chat", (req, res) => {
   let chats = [
@@ -28,5 +34,8 @@ app.get("/api/chat", (req, res) => {
 app.get("/api/chat/:id", (req, res) => {
   res.status(200).send(req.params.id);
 });
+
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`server started on port ${PORT}`.yellow.bold));
